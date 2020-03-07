fcd7b69f8dc69e3f735f52a92cbab28a9809e740
João Bueno <joaovictorbuenodecaldas@gmail.com>
2020-03-07 11:03:35 -0300

    Made mouse getter simpler

--- Assets/Scripts/Interfaces/MousePoint.cs
+++ Assets/Scripts/Interfaces/MousePoint.cs
@@ -5,7 +5,6 @@ namespace GameInterfaces
     public class  MousePoint
     {
         // Mouse Variables
-        private bool clickedOnce  = false;
         private Vector3 mousePos;
 
         public MousePoint(Vector3 initialPos)
@@ -25,28 +24,12 @@ namespace GameInterfaces
                 Physics.Raycast(ray, out hit, Mathf.Infinity);
                 mousePosition = hit.point;
 
-                colliderTag = hit.collider.tag;
-
-                clickedOnce = true;
-
-                Debug.Log($"Hitting { colliderTag }");
-
                 // So that only the background is clickable
-                if(colliderTag.Equals("Background"))
+                if(hit.collider.tag.Equals("Background"))
                 {
                     mousePos = mousePosition;
                 }
             }
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                clickedOnce = false;
-            }
-
-            /*
-             * Vector3 newPos = new Vector3(transform.position.x, transform.position.y, goal.z - distanceFromBackground);
-             * Vector2 mousePos2D = new Vector2(goal.x, goal.y);
-            */
 
             return mousePos;
         }
